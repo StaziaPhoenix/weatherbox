412ac3eb5561e1242f7bf553ba1f5def030c5861
Linh <ldn006@ucsd.edu>
2017-11-20 19:56:27 -0800

    Make esp chip a slave, listening for a 1 (request) from the Rx pin and fetch data at request

--- fetchWeather.ino
+++ fetchWeather.ino
@@ -1,43 +1,32 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <SPI.h>
 #include "Timer.h"
+#include <ESP8266WiFiMulti.h>
 
-#include <ArduinoJson.h>
+static ESP8266WiFiMulti wifiMulti;
+static WiFiClient client;
 
-static PhpocClient phpocClient;
-
-#define INTERVAL 4000
 
 #define HTTP_QUERY_SIZE 1400
+
+// TODO: Edit wifi connection info
+const char* ssid = "UCSD-GUEST";
+const char* pw = "";
+
 uint8_t httpQuery[HTTP_QUERY_SIZE];
 
 int BUTTON_GPIO = 3;
 
 char *server = "api.openweathermap.org";
-char *apiKey = "YOUR_API_KEY";
-char *cityNames[] = {"CITY_NAME,COUNTRY_CODE"};
+char *apiKey = "5d7171bef3c4d2055fcff9885bb2cf66";
+char *cityNames[] = {"San Diego, US"};
 char *currentWeather = "GET /data/2.5/weather?";
 
-// Group 2xx: Thunderstorm  weather5.png
-// Group 3xx: Drizzle  weather5.png
-// Group 5xx: Rain  weather5.png
-
-// Group 6xx: Snow  weather7.png
-
-// Group 7xx: Atmosphere(안개)  weather2.png
-
-// Group 800: CLEAR SKY. weather1.png
-
-// Group 801: FEW CLOUDS. weather4.png
-// Group 802: SCATTERED CLOUDS. weather2.png
-// Group 803: BROKEN CLOUDS. weather3.png
-// Group 804: OVERCAST CLOUDS. weather3.png
 
 uint16_t queryLength = 0;
 bool isRunning = false;
 String httpResponse = "";
-int numberOfCity = 0;
+int numberOfCity = 1;
 
 void setup() {
   // put your setup code here, to run once:  
@@ -45,52 +34,41 @@ void setup() {
   while (!Serial);// wait for serial port to connect.
   Serial.println();
 
-  Serial1.begin(115200);
-  while (!Serial1);// wait for serial port to connect.
+  // Connect to Wifi network
+  client.begin(ssid,pw);
 
-  sendCmd("Clear\r\n");
-  sendCmd("LoadFont \"/unbatang.ttf\"\r\n");
-
-  Phpoc.begin(PF_LOG_SPI | PF_LOG_NET);
-  Serial.println(Phpoc.localIP());
+  // Print the network that the chip is connected to
+  Serial.println(client.localIP());
 
   numberOfCity = sizeof(cityNames) / sizeof(char*);
   
-  // LCD
-  initLCD();
 }
 
 void loop() {  
-  // put your main code here, to run repeatedly:  
-  apiLoop();
-  httpLoop();
+  // while there's is a request from arduino, fetch
+  while (Serial.read() == 1) {
+    apiLoop();
+    httpLoop();
 }
 
 void apiLoop()
 {
   static int index = 0;
-  static unsigned long prevMills = 0;
-  bool callApi = false;
 
-  unsigned long currentMills = millis();
-  if(prevMills == 0 || (currentMills - prevMills >= INTERVAL))
-  {
-    prevMills = currentMills;
-    callApi = true;
+  queryLength = getQueryCurrentWeather(httpQuery, cityNames[index++],
+      apiKey);
+  if(index >= numberOfCity) {
+    index = 0;
   }
-
-  if(callApi)
+    
+  httpResponse = "";
+  
+  // Use object of WiFi client class to create TCP connections
+  if(client.connect(server, 80))
   {
-    queryLength = getQueryCurrentWeather(httpQuery, cityNames[index++], apiKey);
-    if(index >= numberOfCity)
-      index = 0;
-      
-    httpResponse = "";
-    if(phpocClient.connect(server, 80))
-    {
-      isRunning = true;
-      phpocClient.write(httpQuery, queryLength);
-    }
+    isRunning = true;
+    client.write(httpQuery, queryLength);
+  
   }
 }
 
@@ -100,75 +78,35 @@ void httpLoop()
 {
   if(isRunning)  
   {
-    uint16_t cnt = phpocClient.available();
+    // Count the amount of data availble for reading
+    uint16_t cnt = client.available();
+
     if(cnt > 0)
     {
+      // If the number of bytes to be read is greater than 128, just
+      // ready 128 bytes. Otherwise, fill the bytes.
+      //
       uint8_t tryRead = cnt > 128 ? 128 : cnt;
       memset(buf, 0, 1024);
-      uint16_t recv = phpocClient.read(buf, tryRead);
+
+      // Store read bytes in recv
+      uint16_t recv = client.read(buf, tryRead);
       
       httpResponse = httpResponse + buf;      
-      
+
+      Serial.println("Received");
+      Serial.println(recv);
+      //Save index of last occurence of the string f
       int idx = httpResponse.lastIndexOf(f);
       if(idx >= 0)
         httpResponse = httpResponse.substring(idx + 4);
     }
     
-    if(!phpocClient.connected())
+    if(!client.connected())
     {
       isRunning = false;
-      sendCmd("DisableFlush");
+      Serial.println("Client not connected");
       
-      //----------------------------------------
-      // JSON
-      Serial.println(httpResponse.c_str());
-      
-      DynamicJsonBuffer jsonBuffer;
-      JsonObject& root = jsonBuffer.parseObject(httpResponse);
-      if(!root.success())
-      {
-        Serial.println("parseObject failed.");
-        phpocClient.stop();
-        isRunning = false;
-        return;
-      }
-      
-      uint16_t resultCode = root["cod"];
-      Serial.println(resultCode);
-      if(resultCode == 200)
-      {
-        //
-        JsonObject& weather = root["weather"][0];
-        String weatherId = weather["id"]; 
-        String cityName = root["name"]; 
-        double temperature = root["main"]["temp"]; 
-        String temp = String(lround(temperature));
-        String humidity = root["main"]["humidity"]; 
-        String weatherDesc = weather["description"]; 
-
-        printIcon(weatherId);
-        printCity(cityName);
-        printTemperature(temp);
-        printHumidity(humidity);
-        printWeather(weatherDesc);
-        
-        hidePopup();
-      
-        sendCmd("EnableLayer 1");
-        sendCmd("EnableLayer 2");
-        sendCmd("EnableLayer 3");
-        sendCmd("EnableLayer 4");
-        sendCmd("EnableLayer 5");
-      }
-      else
-      {
-        // fail.
-      }
-      //----------------------------------------
-      
-      Serial.println("disconnected");
-      phpocClient.stop();
-      sendCmd("EnableFlush");
     }
   }
 }
@@ -187,7 +125,9 @@ uint16_t writeString(const char* string, uint8_t* buf, uint16_t pos)
     return pos;
 }
 
-uint16_t getQueryCurrentWeather(uint8_t* query, char* cityName, char* apiKey)
+uint16_t getQueryCurrentWeather(uint8_t* query, char* cityName, 
+    char* apiKey)
+
 {
   uint16_t queryLength = 0;
 
@@ -197,171 +137,11 @@ uint16_t getQueryCurrentWeather(uint8_t* query, char* cityName, char* apiKey)
   queryLength = writeString("&units=metric&appid=", httpQuery, queryLength);
   queryLength = writeString(apiKey, query, queryLength);  
 
-  queryLength = writeString(" HTTP/1.1\r\nHost: api.openweathermap.org\r\n", query, queryLength);
-  queryLength = writeString("Connection: Closed\r\n\r\n", query, queryLength);
+  queryLength = writeString(
+      " HTTP/1.1\r\nHost: api.openweathermap.org\r\n", query, queryLength);
+  queryLength = writeString(
+      "Connection: Closed\r\n\r\n", query, queryLength);
   
   return queryLength;
 }
 
-void sendCmd(char* cmd)
-{
-  Serial1.println(cmd);
-}
-
-void initLCD()
-{
-  sendCmd("Layer 0");
-  sendCmd("Clear");
-  //sendCmd("LoadFont \"/unbatang.ttf\"");
-
-  sendCmd("DestroyLayer 1");
-  sendCmd("DestroyLayer 2");
-  sendCmd("DestroyLayer 3");
-  sendCmd("DestroyLayer 4");
-  sendCmd("DestroyLayer 5");
-  sendCmd("DestroyLayer 6");
-  sendCmd("DestroyLayer 7");
-  sendCmd("DestroyLayer 8");
-  sendCmd("DestroyLayer 9");
-  sendCmd("DestroyLayer 10");
-  sendCmd("DestroyLayer 11");
-  sendCmd("DestroyLayer 12");
-  sendCmd("DestroyLayer 13");
-
-  createLayers();
-  createPopup();
-
-  showPopup("Loading...");
-
-  sendCmd("DisableFlush");
-  sendCmd("Layer 0");
-  sendCmd("FontSize 48");  
-  sendCmd("Image 0 0 \"/image/bk3.png\"");
-  sendCmd("EnableFlush");
-}
-
-void createLayers()
-{
-  // weather condition
-  sendCmd("CreateLayer 68 380 732 70 1");  
-  // ICON
-  sendCmd("CreateLayer 68 108 265 245 2");
-  // City name
-  sendCmd("CreateLayer 460 60 320 80 3");
-  // Temperature
-  sendCmd("CreateLayer 460 170 340 120 4");
-  // Humidity
-  sendCmd("CreateLayer 460 310 340 70 5");
-}
-
-void createPopup()
-{  
-  sendCmd("CreateLayer 80 180 640 120 6");
-  sendCmd("Layer 6");
-  sendCmd("DisableLayer 6");
-}
-
-void showPopup(String msg)
-{
-  String cmd;
-  sendCmd("Layer 6");
-  sendCmd("Clear");
-  sendCmd("FontSize 72");
-  sendCmd("Color FFFFFF");
-  cmd = "Button 0 0 640 120 10 336699 336699 \"" + msg + "\"";
-  sendCmd(cmd.c_str());
-  sendCmd("EnableLayer 6");
-}
-
-void hidePopup()
-{
-  sendCmd("DisableLayer 6");
-}
-
-void printCity(String cityName)
-{
-  String cmd;
-  sendCmd("Layer 3");
-  sendCmd("DisableLayer 3");
-  sendCmd("Clear");
-  sendCmd("FontSize 60");
-  sendCmd("Color FFFFFF");
-  cmd = "FillText 0 60 \""; cmd = cmd + cityName + "\"";
-  sendCmd(cmd.c_str());
-}
-
-void printTemperature(String temperature)
-{
-  String cmd;
-  sendCmd("Layer 4");
-  sendCmd("DisableLayer 4");
-  sendCmd("Clear");
-  sendCmd("FontSize 100");
-  sendCmd("Color FFFFFF");
-  cmd = "FillText 0 90 \""; cmd = cmd + temperature + "℃\"";
-  sendCmd(cmd.c_str());
-}
-
-void printHumidity(String humidity)
-{
-  String cmd;
-  sendCmd("Layer 5");
-  sendCmd("DisableLayer 5");
-  sendCmd("Clear");
-  sendCmd("FontSize 48");
-  sendCmd("Color FFFFFF");
-  cmd = "FillText 0 50 \""; cmd = cmd + humidity + "%\"";
-  sendCmd(cmd.c_str());
-}
-
-void printIcon(String id)
-{
-  sendCmd("Layer 2");
-  sendCmd("DisableLayer 2");
-  sendCmd("Clear");
-  
-  String cmd, icon;
-  char code = id.charAt(0);
-  switch(code)
-  {
-    case '2':
-    case '3':
-    case '5':
-      icon = "/image/weather5.png";
-      break;
-    case '6':
-      icon = "/image/weather7.png";
-      break;
-    case '7':
-      icon = "/image/weather2.png";
-      break;
-     case '8':
-      if(id.equals("800"))
-        icon = "/image/weather1.png";
-      else if(id.equals("801"))
-        icon = "/image/weather4.png";
-      else if(id.equals("802"))
-        icon = "/image/weather2.png";
-      else if(id.equals("803"))
-        icon = "/image/weather3.png";
-      else if(id.equals("804"))
-        icon = "/image/weather3.png";
-      break;
-  }
-
-  cmd = "Image 0 0 \"" + icon + "\"";
-  sendCmd(cmd.c_str());
-}
-
-void printWeather(String weather)
-{
-  String cmd;
-  sendCmd("Layer 1");
-  sendCmd("DisableLayer 1");
-  sendCmd("Clear");
-  sendCmd("FontSize 48");
-  sendCmd("Color FFFFFF");
-  weather.toUpperCase();
-  cmd = "FillText 0 50 \""; cmd = cmd + weather + "\"";
-  //sendCmd(cmd.c_str());
-}
